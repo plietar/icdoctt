f4a6d7dc50728ec149ca7a8332c71f9c811e0d01
Paul Liétar <paul@lietar.net>
2014-09-09 01:34:06 +0200

    Generate ical file based on parsed timetable.

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from urllib.request import urlopen
 import bs4 as BeautifulSoup
 import requests
 import re
+import icalendar as ical
 import datetime as dt
+import sys
 
 WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
 termstart = dt.date(2014, 10, 6)
 
+cal = ical.Calendar()
+
 soup = BeautifulSoup.BeautifulSoup(requests.get('http://www.doc.ic.ac.uk/internal/timetables/2014-15/autumn/class/9_2_10.htm').text)
 table = soup.find('table')
 body = table.tbody
 
-print(soup.find_all('h3')[1].get_text().strip())
-print()
+cal['summary'] = soup.find_all('h3')[1].get_text().strip()
+
+print(soup.find_all('h3')[1].get_text().strip(), file=sys.stderr)
+print(file=sys.stderr)
 
 for tr in body.find_all('tr'):
     tds = [ [s for s in td.strings if not s.isspace()] for td in tr.find_all('td') ]
@@ -28,14 +34,24 @@ for tr in body.find_all('tr'):
             for w in range(int(m.group(2)), int(m.group(3)) + 1):
                 t = dt.datetime.combine(termstart + dt.timedelta(weeks = (w - 1), days = i), time)
                 
-                print(td[0])
-                print(t)
+                print(td[0], file=sys.stderr)
+                print(t, file=sys.stderr)
 
                 if m.group(1) != 'Wks':
-                    print(m.group(1))
+                    print(m.group(1), file=sys.stderr)
                 if m.group(4):
-                    print('%s' % m.group(4))
+                    print('%s' % m.group(4), file=sys.stderr)
                 if m.group(5):
-                    print('Room %s' % m.group(5))
-                print()
+                    print('Room %s' % m.group(5), file=sys.stderr)
+                print(file=sys.stderr)
+
+                event = ical.Event()
+                event.add('summary', td[0])
+                event.add('dtstart', t)
+                event.add('dtend', t + dt.timedelta(hours = 1))
+                if m.group(5):
+                    event['location'] = ical.vText('Room %s' % m.group(5))
+                cal.add_component(event)
+                
+sys.stdout.buffer.write(cal.to_ical())
 
